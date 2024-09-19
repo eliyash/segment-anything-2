1504c0476a56f9ddda6af2f6e5b0419a2fa4fec2
eliyash <eliyash3@gmail.com>
2024-09-19 13:12:59 +0300

    reorder show_monkey_code

--- florence_2/show_results.py
+++ florence_2/show_results.py
@@ -3,26 +3,71 @@ from pathlib import Path
 
 import cv2
 
+ALL_COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (125, 120, 0), (0, 125, 120), (120, 0, 125), (85, 85, 85)]
+ALL_BODY_PARTS = ['chimpanzee', 'face', 'ear']
+COLORS_DICT = {part: ALL_COLORS[color_ind] for color_ind, part in enumerate(ALL_BODY_PARTS)}
 
-def monkey_show(image, bboxs, colors=None):
-    if colors is None:
-        colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (125, 120, 0), (0, 125, 120), (120, 0, 125), (85, 85, 85)]
 
+def look_for_match(holding_bbox, bboxs_to_check):
+    (hl, ht, hr, hb) = holding_bbox
+    hw = hr - hl
+    hh = hb - ht
+    (el, et, er, eb) = (hl - hw // 4, ht - hh // 4, hr + hw // 4, hb + hh // 4)
+    matches = []
+    for bbox_to_check in bboxs_to_check:
+        (cl, ct, cr, cb) = bbox_to_check
+        cw = cr - cl
+        ch = cb - ct
+        if cw < hw / 2 and ch < hh / 2 and el <= cl and et <= ct and er >= cr and eb >= cb:
+            matches.append(bbox_to_check)
+    return matches
+
+
+def monkey_show_filtered(image, mapped_parts_for_each_chimpanzee: list):
     image_with_boxes = image.copy()
 
-    for i, (l, t, r, b) in enumerate(bboxs['<CAPTION_TO_PHRASE_GROUNDING>']['bboxes']):
-        (l, t, r, b) = map(int, (l, t, r, b))
-        cv2.rectangle(image_with_boxes, (l, t), (r, b), colors[i % len(colors)], 2)
+    for i, individual_parts in enumerate(mapped_parts_for_each_chimpanzee):
+        for type_of_part, bboxs in individual_parts.items():
+            for bbox in bboxs:
+                (l, t, r, b) = map(int, bbox)
+                cv2.rectangle(image_with_boxes, (l, t), (r, b), COLORS_DICT[type_of_part], 2)
 
     return image_with_boxes
 
 
+def validate_data(dict_of_all_bbox_dicts):
+    # check if bbox of face at least 2 times smaller than chimpanzee (full body) in each axis
+    # check if bbox of ear at least 2 times smaller than bbox of face in each axis
+    # stages: frst we will match each full body to face and ear checking face and ears bbox are mostly inside full body bbox
+    # after we got match for each chimpanzee parts, we will check soze validity
+    full_body_bboxs = dict_of_all_bbox_dicts['chimpanzee']['<CAPTION_TO_PHRASE_GROUNDING>']['bboxes']
+    face_bboxs = dict_of_all_bbox_dicts['face']['<CAPTION_TO_PHRASE_GROUNDING>']['bboxes']
+    ear_bboxs = dict_of_all_bbox_dicts['ear']['<CAPTION_TO_PHRASE_GROUNDING>']['bboxes']
+    mapped_parts_for_each_chimpanzee = []
+    for full_body_bbox in full_body_bboxs:
+        matched_face_bboxs = look_for_match(full_body_bbox, face_bboxs)
+        matched_ear_bboxs = []
+        for face_bbox in face_bboxs:
+            matched_ear_bboxs.extend(look_for_match(face_bbox, ear_bboxs))
+        mapped_parts_for_each_chimpanzee.append({'chimpanzee': [full_body_bbox], 'face': matched_face_bboxs, 'ear': matched_ear_bboxs})
+
+    return mapped_parts_for_each_chimpanzee
+
+
+def get_all_annotation_paths(annotation_root, current_frame_ind, video_path):
+    all_annotation_file_paths = {}
+    for annotation_type in annotation_root.iterdir():
+        if annotation_type.name not in COLORS_DICT:
+            continue
+        annotation_path = annotation_type / video_path.stem
+        annotation_file_path = annotation_path / f'bboxs_{current_frame_ind}.json'
+        all_annotation_file_paths[annotation_type.name] = annotation_file_path
+    return all_annotation_file_paths
+
+
 def show_florence2_results(video_root_path, annotation_root):
-    for video_path in video_root_path.iterdir():
-
-        body_annotation_path = annotation_root / video_path.stem
-        face_annotation_path = annotation_root / 'face' / video_path.stem
-
+    # for video_path in video_root_path.iterdir():
+        video_path = video_root_path / '5_21_19 (1).MTS'
         # Open the video file
         video_capture = cv2.VideoCapture(video_path.as_posix())  # Replace with your video file path
 
@@ -31,29 +76,36 @@ def show_florence2_results(video_root_path, annotation_root):
             print("Error opening video file")
 
         # Loop through the video frames
-        i = 0
+        current_frame_ind = 1000
+        # start from frame i
+        # Set the frame number to start from
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, current_frame_ind)
+
         while True:
             # Read the next frame
             ret, frame = video_capture.read()
             # If there are no more frames, break the loop
             if not ret:
                 break
-            body_file_path = body_annotation_path / f'bboxs_{i}.json'
-            face_file_path = face_annotation_path / f'bboxs_{i}.json'
-            if not body_file_path.exists() or not face_file_path.exists():
+
+            all_annotation_file_paths = get_all_annotation_paths(annotation_root, current_frame_ind, video_path)
+
+            current_frame_ind += 1
+
+            if not all([f.exists() for f in all_annotation_file_paths.values()]):
                 break
-            body_res_dict = json.loads(body_file_path.read_text())
-            face_res_dict = json.loads(face_file_path.read_text())
-            frame_with_bboxs = monkey_show(frame, body_res_dict, [(255, 0, 0)])
-            frame_with_bboxs = monkey_show(frame_with_bboxs, face_res_dict, [(0, 0, 255)])
+            res_dicts = {n: json.loads(f.read_text()) for n, f in all_annotation_file_paths.items()}
+
+            list_of_data = validate_data(res_dicts)
+            frame_with_bboxs = monkey_show_filtered(frame, list_of_data)
             cv2.imshow('image', frame_with_bboxs)
-            cv2.waitKey(100)
-            i += 1
+            cv2.waitKey(10)
 
 
 def main():
-    video_root_path = Path(r'D:\videos_2019')
-    annotation_root = Path(r'C:\Workspace\ChimpanzeesThesis\outputs\florence2_current\florence2')
+    drive_path = Path('D:/') if Path('D:/').exists() else Path('E:/')
+    video_root_path = drive_path / 'videos_2019'
+    annotation_root = Path(r'C:\Workspace\ChimpanzeesThesis\outputs\florence2_17_9_24\home\ubuntu\segment-anything-2\florence_2\output\florence2')
 
     show_florence2_results(video_root_path, annotation_root)
 
