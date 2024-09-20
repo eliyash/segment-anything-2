b4d4ca1476d7a0c5b95f462826d939d9973f7c98
eliyash <eliyash3@gmail.com>
2024-09-20 16:20:12 +0300

    update code of training facenet

--- facenet/facenet_fine_tune.py
+++ facenet/facenet_fine_tune.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from pathlib import Path
 
@@ -11,84 +12,69 @@ from torchvision import datasets, transforms
 import numpy as np
 import os
 
-root_dir = Path(r'/home/ubuntu/faces_work')
-data_dir = root_dir / 'individual_faces_dataset'
-out_dir = root_dir / 'training'
 
-batch_size = 32
-epochs = 8
-workers = 0 if os.name == 'nt' else 8
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Running on device: {}'.format(device))
+def main():
+    is_windows = os.name == 'nt'
 
-train_name = datetime.now().strftime('train__%Y%m%d_%H%M%S')
+    root_dir = Path(r'C:\Workspace\ChimpanzeesThesis\faces_images') if is_windows else Path(r'/home/ubuntu/faces_work')
+    data_dir = root_dir / 'individual_faces_dataset'
+    out_dir = root_dir / 'training'
 
-model_folder = out_dir / train_name
-# get_time_formated for file name
+    batch_size = 32
+    epochs = 8
+    workers = 0 if is_windows else 8
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('Running on device: {}'.format(device))
 
+    train_name = datetime.now().strftime('train__%Y%m%d_%H%M%S')
 
-trans = transforms.Compose([
-    np.float32,
-    transforms.ToTensor(),
-    transforms.Resize((512, 512)),
-    fixed_image_standardization
-])
-dataset = datasets.ImageFolder(data_dir.as_posix(), transform=trans)
-resnet = InceptionResnetV1(
-    classify=True,
-    pretrained='vggface2',
-    num_classes=len(dataset.class_to_idx)
-).to(device)
+    model_folder = out_dir / train_name
+    model_folder.mkdir(parents=True, exist_ok=True)
 
-optimizer = optim.Adam(resnet.parameters(), lr=0.001)
-scheduler = MultiStepLR(optimizer, [5, 10])
+    trans = transforms.Compose([
+        np.float32,
+        transforms.ToTensor(),
+        transforms.Resize((512, 512)),
+        fixed_image_standardization
+    ])
+    dataset = datasets.ImageFolder(data_dir.as_posix(), transform=trans)
+    resnet = InceptionResnetV1(
+        classify=True,
+        pretrained='vggface2',
+        num_classes=len(dataset.class_to_idx)
+    ).to(device)
 
-img_inds = np.arange(len(dataset))
-np.random.shuffle(img_inds)
-train_inds = img_inds[:int(0.8 * len(img_inds))]
-val_inds = img_inds[int(0.8 * len(img_inds)):]
+    optimizer = optim.Adam(resnet.parameters(), lr=0.001)
+    scheduler = MultiStepLR(optimizer, [5, 10])
 
-train_loader = DataLoader(
-    dataset,
-    num_workers=workers,
-    batch_size=batch_size,
-    sampler=SubsetRandomSampler(train_inds)
-)
-val_loader = DataLoader(
-    dataset,
-    num_workers=workers,
-    batch_size=batch_size,
-    sampler=SubsetRandomSampler(val_inds)
-)
-loss_fn = torch.nn.CrossEntropyLoss()
-metrics = {
-    'fps': training.BatchTimer(),
-    'acc': training.accuracy
-}
+    img_inds = np.arange(len(dataset))
+    np.random.shuffle(img_inds)
+    train_inds = img_inds[:int(0.8 * len(img_inds))]
+    val_inds = img_inds[int(0.8 * len(img_inds)):]
 
-writer = SummaryWriter()
-writer.iteration, writer.interval = 0, 10
-
-print('\n\nInitial')
-print('-' * 10)
-resnet.eval()
-training.pass_epoch(
-    resnet, loss_fn, val_loader,
-    batch_metrics=metrics, show_running=True, device=device,
-    writer=writer
-)
-
-for epoch in range(epochs):
-    print('\nEpoch {}/{}'.format(epoch + 1, epochs))
-    print('-' * 10)
-
-    resnet.train()
-    training.pass_epoch(
-        resnet, loss_fn, train_loader, optimizer, scheduler,
-        batch_metrics=metrics, show_running=True, device=device,
-        writer=writer
+    train_loader = DataLoader(
+        dataset,
+        num_workers=workers,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(train_inds)
     )
+    val_loader = DataLoader(
+        dataset,
+        num_workers=workers,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(val_inds)
+    )
+    loss_fn = torch.nn.CrossEntropyLoss()
+    metrics = {
+        'fps': training.BatchTimer(),
+        'acc': training.accuracy
+    }
 
+    writer = SummaryWriter()
+    writer.iteration, writer.interval = 0, 10
+
+    print('\n\nInitial')
+    print('-' * 10)
     resnet.eval()
     training.pass_epoch(
         resnet, loss_fn, val_loader,
@@ -96,7 +82,29 @@ for epoch in range(epochs):
         writer=writer
     )
 
-# save netiwrk to
-model_folder.mkdir(parents=True, exist_ok=True)
-torch.save(resnet.state_dict(), model_folder / 'model.pt')
-writer.close()
+    for epoch in range(epochs):
+        print('\nEpoch {}/{}'.format(epoch + 1, epochs))
+        print('-' * 10)
+
+        resnet.train()
+        training.pass_epoch(
+            resnet, loss_fn, train_loader, optimizer, scheduler,
+            batch_metrics=metrics, show_running=True, device=device,
+            writer=writer
+        )
+
+        resnet.eval()
+        training.pass_epoch(
+            resnet, loss_fn, val_loader,
+            batch_metrics=metrics, show_running=True, device=device,
+            writer=writer
+        )
+        info = {'epoch': epoch}
+        (model_folder / 'info.json').write_text(json.dumps(info, indent=4))
+        torch.save(resnet.state_dict(), model_folder / 'model.pt')
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    main()
