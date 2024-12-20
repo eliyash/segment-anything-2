d7249a0e31575d138c2c6cedf16ae68f22c87476
eliyash <eliyash3@gmail.com>
2024-12-20 14:40:20 +0200

    add task

--- yolov9-main/train_dual.py
+++ yolov9-main/train_dual.py
@@ -641,4 +641,19 @@ def run(**kwargs):
 
 if __name__ == "__main__":
     opt = parse_opt()
+    from clearml import Task
+
+    task = Task.init(
+        project_name='Chimps',  # project name of at least 3 characters
+        task_name='a',  # task name of at least 3 characters
+        task_type=None,
+        tags=None,
+        reuse_last_task_id=True,
+        continue_last_task=False,
+        output_uri=None,
+        auto_connect_arg_parser=True,
+        # auto_connect_frameworks=True,
+        # auto_resource_monitoring=True,
+        # auto_connect_streams=True,
+    )
     main(opt)
