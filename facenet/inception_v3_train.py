fa648f14d25e5977050fb6531ade6e0c1b48453e
eliyash <eliyash3@gmail.com>
2025-01-10 00:23:25 +0200

    update face classifier

--- facenet/inception_v3_train.py
+++ facenet/inception_v3_train.py
@@ -1,147 +1,166 @@
 import torch
 import torchvision.models as models
-from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset, random_split
-from PIL import Image
+from torch.utils.data import DataLoader, Dataset
 from pathlib import Path
 import schedulefree
 from torchvision.models import Inception_V3_Weights
+from torchvision import transforms
+
+from PIL import Image
+
+from yolov9_main.monkey_names_with_classes import ALL_NAMES_TO_CLASS_INDEX
 
 
-class FaceDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = Path(root_dir)
+# Dataset definition
+class ChimpFaceDataset(Dataset):
+    def __init__(self, root_dir: Path, transform=None):
+        self.root_dir = root_dir
         self.transform = transform
         self.image_paths = []
         self.labels = []
 
-        for label, person_folder in enumerate(self.root_dir.iterdir()):
-            for image_path in person_folder.iterdir():
-                self.image_paths.append(image_path)
-                self.labels.append(label)
+        # Traverse folders to collect image paths and labels
+        for index, individual_folder in enumerate(sorted(root_dir.iterdir())):
+            if individual_folder.is_dir():
+                label = ALL_NAMES_TO_CLASS_INDEX[individual_folder.name.split('_')[-1]]
+                for img_file in individual_folder.iterdir():
+                    self.image_paths.append(img_file)
+                    self.labels.append(label)
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        image = Image.open(self.image_paths[idx]).convert('RGB')
+        img_path = self.image_paths[idx]
         label = self.labels[idx]
+        image = Image.open(img_path).convert("RGB")
 
         if self.transform:
             image = self.transform(image)
 
         return image, label
 
-# Data transformations
-from torchvision import transforms
 
-data_transform = transforms.Compose([
-    transforms.Resize((299, 299)),
+def main(load_best_model=True):
+    batch_size = 32
+    root_data_dir = Path(r'D:\faces_dataset')
+    out_dir = Path(r'D:\training_output\inception_v3_train')
+    out_dir.mkdir(parents=True, exist_ok=True)
 
-    # --- Standard Augmentations ---
-    transforms.RandomApply([
-        transforms.RandomHorizontalFlip(),  # Flip horizontally
-        transforms.RandomRotation(degrees=15),  # Rotate by up to 15 degrees
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),  # Translate and scale
-    ], p=0.8),
+    train_transform = transforms.Compose([
+        transforms.Resize((299, 299)),
 
-    # --- Augmentations to reduce image quality ---
-    transforms.RandomApply([
-        transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
-        transforms.RandomAdjustSharpness(sharpness_factor=0.5, p=0.5),
-    ], p=0.8),
+        # --- Standard Augmentations ---
+        transforms.RandomApply([
+            transforms.RandomHorizontalFlip(),  # Flip horizontally
+            transforms.RandomRotation(degrees=15),  # Rotate by up to 15 degrees
+            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),  # Translate and scale
+        ], p=0.8),
 
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
+        # --- Augmentations to reduce image quality ---
+        transforms.RandomApply([
+            transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2),
+            transforms.RandomAdjustSharpness(sharpness_factor=0.5, p=0.5),
+        ], p=0.8),
 
-# Create dataset
-data_dir = Path(r'C:\Workspace\ChimpanzeesThesis\faces_images\individual_faces_dataset')
-out_dir = Path(r'C:\Workspace\ChimpanzeesThesis\outputs\inception_v3_train')
-full_dataset = FaceDataset(root_dir=data_dir, transform=data_transform)
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-# Split into train and validation sets
-val_size = int(0.2 * len(full_dataset))
-train_size = len(full_dataset) - val_size
-train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    val_transform = transforms.Compose([
+        transforms.Resize((299, 299)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
 
-# Create dataloaders
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32)  # No need to shuffle validation data
+    train_dataset = ChimpFaceDataset(root_dir=root_data_dir / 'train', transform=train_transform)
+    val_dataset = ChimpFaceDataset(root_dir=root_data_dir / 'val', transform=val_transform)
 
-# --- Model ---
+    # Create dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, drop_last=True)
 
-# Load a pre-trained model (InceptionResnetV1)
-model = models.inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)
+    # --- Model ---
 
-# Modify the final classification layer
-num_classes = len(list(data_dir.iterdir()))  # Number of people
-model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+    # Load a pre-trained model (InceptionResnetV1)
+    model = models.inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)
 
-load_best_model = True
-if load_best_model:
-    model.load_state_dict(torch.load(out_dir / 'best_face_recognition_model_with_augmentations.pth', weights_only=True))
+    # Modify the final classification layer
+    num_classes = max(ALL_NAMES_TO_CLASS_INDEX) + 1  # Number of people
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
 
-# --- Training ---
+    if load_best_model and (out_dir / 'best_model.pth').exists():
+        model.load_state_dict(torch.load(out_dir / 'best_model.pth', weights_only=True))
 
-# Define optimizer and loss function
-# optimizer = torch.optim.Adam(model.parameters())
-optimizer = schedulefree.AdamWScheduleFree(model.parameters())
-criterion = torch.nn.CrossEntropyLoss()
+    # --- Training ---
 
-# Initialize variables to track best validation loss
-best_val_loss = float('inf')
+    # Define optimizer and loss function
+    # optimizer = torch.optim.Adam(model.parameters())
+    optimizer = schedulefree.AdamWScheduleFree(model.parameters())
+    criterion = torch.nn.CrossEntropyLoss()
 
-# Training loop
-num_epochs = 1000  # Adjust as needed
-for epoch in range(num_epochs):
-    # Validation phase
-    model.eval()   # Set model to evaluation mode
-    optimizer.eval()
-    val_loss = 0.0
-    correct_classifications = 0
-    total_samples = 0
-    with torch.no_grad():
-        for images, labels in val_loader:
+    # Initialize variables to track best validation loss
+    best_val_loss = float('inf')
+
+    # Training loop
+    num_epochs = 1000  # Adjust as needed
+    for epoch in range(num_epochs):
+        # Validation phase
+        model.eval()   # Set model to evaluation mode
+        optimizer.eval()
+        val_loss = 0.0
+        correct_classifications = 0
+        total_samples = 0
+        with torch.no_grad():
+            for images, labels in val_loader:
+                outputs = model(images)
+                # outputs = outputs.logits
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+
+                # Calculate accuracy
+                _, predicted = torch.max(outputs.data, 1)
+                total_samples += labels.size(0)
+                correct_classifications += (predicted == labels).sum().item()
+
+        val_loss /= len(val_loader)
+        accuracy = 100 * correct_classifications / total_samples
+
+        print(
+            f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {accuracy:.2f}%'
+        )
+
+        # Save the model if it has the best validation loss so far
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), out_dir / 'best_model.pth')
+            print("saving model...")
+
+        # Training phase
+        model.train()  # Set model to training mode
+        optimizer.train()
+        # i = 0
+        for images, labels in train_loader:
+            # i += 1
+            # if i < 18 * 4 + 1:
+            #     continue
+
+            # Forward pass
             outputs = model(images)
-            # outputs = outputs.logits
+            outputs = outputs.logits
+
             loss = criterion(outputs, labels)
-            val_loss += loss.item()
 
-            # Calculate accuracy
-            _, predicted = torch.max(outputs.data, 1)
-            total_samples += labels.size(0)
-            correct_classifications += (predicted == labels).sum().item()
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    val_loss /= len(val_loader)
-    accuracy = 100 * correct_classifications / total_samples
+            print(f'.', end='')
 
-    print(
-        f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {accuracy:.2f}%'
-    )
+    print("Training complete. Best model saved as 'best_face_recognition_model.pth'")
 
-    # Save the model if it has the best validation loss so far
-    if val_loss < best_val_loss:
-        best_val_loss = val_loss
-        torch.save(model.state_dict(), out_dir / 'best_face_recognition_model_with_augmentations.pth')
-        print("saving model...")
 
-    # Training phase
-    model.train()  # Set model to training mode
-    optimizer.train()
-    for images, labels in train_loader:
-        # Forward pass
-        outputs = model(images)
-        outputs = outputs.logits
-
-        loss = criterion(outputs, labels)
-
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        print(f'.', end='')
-
-print("Training complete. Best model saved as 'best_face_recognition_model.pth'")
+if __name__ == '__main__':
+    main()
