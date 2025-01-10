c0c56a781c9ea1730f791b7b4084b3230498822b
eliyash <eliyash3@gmail.com>
2025-01-10 11:01:09 +0200

    create params script

--- facenet/create_params.py
+++ facenet/create_params.py
@@ -1,21 +1,21 @@
 import argparse
 
 from pathlib import Path
-from clearml import Task
+# from clearml import Task
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_path', type=str, default=Path(r'/training_output/inception_v3_train'))
-    parser.add_argument('--data_path', type=str, default=Path(r'/faces_dataset'))
+    parser.add_argument('--output_path', type=str)
+    parser.add_argument('--data_path', type=str)
     parser.add_argument('--batch_size', type=int, default=32, help='batch size')
     parser.add_argument('--num_epochs', type=int, default=1000, help='number of epochs')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    task = Task.init(auto_connect_arg_parser=True)
+    # task = Task.init(auto_connect_arg_parser=True)
 
     print(parse_opt())
-    task.connect(parse_opt().__dict__)
+    # task.connect(parse_opt().__dict__)
 
