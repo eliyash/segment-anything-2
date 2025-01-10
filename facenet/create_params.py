995b8ae1272f5f65f23cf7e8b01f19513188043a
eliyash <eliyash3@gmail.com>
2025-01-10 10:31:02 +0200

    create params script

---
+++ facenet/create_params.py
@@ -0,0 +1,16 @@
+import argparse
+
+from pathlib import Path
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_path', type=str, default=Path(r'/training_output/inception_v3_train'))
+    parser.add_argument('--data_path', type=str, default=Path(r'/faces_dataset'))
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size')
+    parser.add_argument('--num_epochs', type=int, default=1000, help='number of epochs')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    print(parse_opt().__dict__)
